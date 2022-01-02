46356ce51cafbf0e4ff9cc9f5705ea27045ea415
Preyash-tryin-to-code <preyashyadav29@gmail.com>
2022-01-02 16:39:01 +0530

    .,,,

--- main.js
+++ main.js
@@ -51,24 +51,6 @@ function remove(){
 
 
 
-$(document).ready(function() {
-    var max_fields      = 10; //maximum input boxes allowed
-    var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
-    var add_button      = $(".add_field_button"); //Add button ID
-    
-    var x = 1; //initlal text box count
-    $(add_button).click(function(e){ //on add input button click
-    e.preventDefault();
-        if(x < max_fields){ //max input box allowed
-            x++; //text box increment
-            $(wrapper).append('<div class="input-group mb-3"><input placeholder="Enter Price" type="text" name="mytext[]" class="form-control"><div class="input-group-append"><button class="btn btn-outline-danger remove_field" type="button">Remove</button></div></div>'); //add input box
-        }
-    });
-    
-    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-        e.preventDefault(); $(this).parent('div').parent('div').remove(); x--;
-        })
-    });
 
 // // eanble disable input field
 // function EnableDisableTextBox(btnPassport) {
