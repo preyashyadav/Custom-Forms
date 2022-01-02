255eb0febec2388c770a3f2ed621779e7df94817
Preyash-tryin-to-code <preyashyadav29@gmail.com>
2022-01-03 01:15:42 +0530

    .

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //toggle view
 
+
+
 function myText() {
     var x = document.getElementById("text");
     if (x.style.display === "none") {
@@ -29,4 +31,30 @@ function myCheckBox(){
       x.style.display = "none";
     }
 }
+
+
+
+
+// function selectIt(){
+//   var q= document.getElementById("text");
+//   var r= document.getElementById("textArea");
+//   var s= document.getElementById("checkBox");
+
+//   if(q.style.display==="none"){
+//     q.style.display="block";
+//   }
+//   else if(r.style.display==="none"){
+//     r.style.display="block";
+//   }
+//   else if(s.style.display==="none"){
+//     s.style.display="block";
+//   }
+//   else {
+//     q.style.display = "none";
+//     r.style.display = "none";
+//     s.style.display = "none";
+//   }
+
+// }
+
 
